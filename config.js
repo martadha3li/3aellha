8325582bca3b54199ad1288729406b2b08fa7cc8
martadha3li <martadha3li@gmail.com>
2026-05-27 21:09:20 +0300

    Update config.js

--- config.js
+++ config.js
@@ -1,8 +1,8 @@
-// config.js
+// config.js - Ver: 206-9
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-app.js";
 import { getFirestore } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-firestore.js";
 
-// ضع بيانات مشروعك الخاص بـ Firebase هنا
+// [الربط المركزي المعتمد] - ضع بيانات مشروع فايربيس الخاص بك هنا بدقة
 const firebaseConfig = {
     apiKey: "ضع_هنا_API_KEY",
     authDomain: "ضع_هنا_AUTH_DOMAIN",
@@ -12,6 +12,8 @@ const firebaseConfig = {
     appId: "ضع_هنا_APP_ID"
 };
 
-// تهيئة الخدمة والاتصال بقاعدة البيانات
+// تهيئة الخدمة والاتصال بقاعدة البيانات السحابية بشكل صريح
 const app = initializeApp(firebaseConfig);
+
+// تصدير متغير db ليكون متاحاً للاستدعاء الفوري في بقية ملفات المنصة
 export const db = getFirestore(app);
